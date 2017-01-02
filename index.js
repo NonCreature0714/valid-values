f94dcd29337a5224bd8e257adf693f373233f380
Bill Brubaker <brubaker.bill@gmail.com>
2017-01-02 15:25:38 -0600

    created string validator

---
+++ index.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+//TODO: create simple validators for primatives.
+//TODO: create validators for other Types.
+
+const isArrayOfOneElement = (array: Array()) => {
+	//TODO: complete, improve
+	return array.length === 1;
+}
+
+
+const isAString = (value) => {
+	return typeof value === 'string' || value instanceof String;
+}
+
+const isFile = (ofType, atPath) => {
+	//TODO: complete	
+}
+
+const isADirectory = (filePath) => {
+	//TODO: complete
+}
+
+
+const isFileOrDirectory = (filePath) => {
+	//TODO: complete
+}
+
+const isFoldOfAtLeastOne = (fileType, atPath) => {
+	//TODO: complete
+}
